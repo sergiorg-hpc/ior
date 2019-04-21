04bf7c19c54f8a17d68319becc0467f0bf9a213c
Sergio <sergiorg@kth.se>
2019-04-21 19:52:37 +0200

    Fixed an issue with GPFS on Cori and included an option to clean the compilation using the "build.sh" script.

--- build.sh
+++ build.sh
@@ -4,20 +4,31 @@ IORDIR=`cd $(dirname "$0") && pwd`
 BUILDDIR="$IORDIR/build"
 BINDIR=$IORDIR/../build/bin
 UMMAPIO=$IORDIR/../ummap-io/build
+MAKEFILE_CHECK=`ls $IORDIR/src/Makefile 2>/dev/null`
 
-export CC=$1
-export MPICC=$1
-export CFLAGS="-I$UMMAPIO/include"
-export LDFLAGS="-L$UMMAPIO/lib"
-export LIBS="-lummapio"
-
-cd $IORDIR
-
-if [[ `ls $IORDIR/src/Makefile 2>/dev/null` == "" ]]
+if [[ $1 == "make" ]]
 then
-    ./bootstrap
-    ./configure --with-mmap --prefix=$BUILDDIR
-fi
+    export CC=$2
+    export MPICC=$2
+    export CFLAGS="-I$UMMAPIO/include"
+    export LDFLAGS="-L$UMMAPIO/lib"
+    export LIBS="-lummapio"
 
-make CC="$CC" MPICC="$MPICC" && cp $IORDIR/src/ior $BINDIR/ior.out
+    cd $IORDIR
+
+    if [[ $MAKEFILE_CHECK == "" ]]
+    then
+        ./bootstrap
+        ./configure --with-mmap --without-gpfs --prefix=$BUILDDIR
+    fi
+
+    make CC="$CC" MPICC="$MPICC" && cp $IORDIR/src/ior $BINDIR/ior.out
+else
+    cd $IORDIR
+
+    if [[ $MAKEFILE_CHECK != "" ]]
+    then
+        make clean
+    fi
+fi
 
